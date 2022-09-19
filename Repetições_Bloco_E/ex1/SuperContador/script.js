c7a0674062edc79fce55cdf61c61581747984935
Gabriel Ribeiro da Silva Neto <gabriel.ribeiro@tecnospeed.com.br>
2022-09-19 18:07:30 -0300

    Crescente e descrescente

--- Repetições_Bloco_E/ex1/SuperContador/script.js
+++ Repetições_Bloco_E/ex1/SuperContador/script.js
@@ -12,10 +12,18 @@ function contar() {
        var f = Number(fim.value)
        var p = Number(passo.value)
 
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} `
-        }
+       /* if (i < f) {
+            //Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+            } 
+        } else {
+            //Contagem decrescente*/
+            for (let c = i; c <= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+            } res.innerHTML += `\u{1F3C1}`
+            
+        
+} 
 
-    }
-
-}
